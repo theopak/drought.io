941e2a40292440b3b36b6ab9c1b79d2b7f057f03
Theodore X. Pak <mail@theopak.com>
2014-11-09 22:58:08 -0500

    wip

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,29 @@
  * Controller of the droughtioApp
  */
 angular.module('droughtioApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, $http) {
+    
+    // Get annual/normals data from all stations via NOAA NCDC
+    $scope.getNormalsData = function () {
+      $http({
+        method: 'GET',
+        headers: { 'token': 'kjslmSNkMbvnPHEMUqxlAiKcBlpERRzr' },
+        url: 'http://www.ncdc.noaa.gov/cdo-web/api/v2/datasets/ANNUAL',
+      }).
+      success(function (data) {
+        // attach this data to the scope
+        //$scope.data = reformatNoaaResponse(data);
+        console.log($scope.data);
+
+        // clear the error messages
+        $scope.error = '';
+      }).
+      error(function (data, status) {
+        $scope.error = 'Error: ' + status;
+      });
+    };
+
+    // Get data for the first time
+    $scope.getNoaaData();
+
   });
