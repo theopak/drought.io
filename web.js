607892bee5de4388d7a54ae90803f9e5fdb06402
Theodore X. Pak <mail@theopak.com>
2014-11-10 01:20:24 -0500

    Revert "Uses `express-logger`."
    This reverts commit 9bb0ad82177c4404873dacddd238a81156d1c300.

--- web.js
+++ web.js
@@ -1,8 +1,7 @@
 var gzippo = require('gzippo');
 var express = require('express');
-var logger = require('express-logger');
 var app = express();
  
-app.use(logger('dev'));
+//app.use(express.logger('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(process.env.PORT || 5000);
