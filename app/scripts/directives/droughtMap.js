5c4e161b4a678f9f6f4952880adaa5e8428646da
Theodore X. Pak <mail@theopak.com>
2014-11-20 10:05:20 -0500

    Adds interactivity to map thanks to the excellent example at http://bl.ocks.org/mbostock/2206340

--- app/scripts/directives/droughtMap.js
+++ app/scripts/directives/droughtMap.js
@@ -13,11 +13,11 @@ app.directive('droughtMap', ['d3Service',
       link: function (scope, element, attrs) {
         console.log('inside drought map!!!');
         d3Service.d3().then(function(d3) {      
-            var width = 960,
-                height = 500;
+            // var width = 960,
+            //     height = 500;
 
-            var svgWidth = window.innerWidth - (window.innerWidth / 3),
-                svgHeight = window.innerHeight - 20;
+            var width = window.innerWidth - (window.innerWidth / 3),
+                height = window.innerHeight - 20;
 
             var projection = d3.geo.albersUsa()
                 // .translate([50, svgHeight/2 - 95])
@@ -27,50 +27,71 @@ app.directive('droughtMap', ['d3Service',
                 // .scale([4000])
                 // .rotate([123,0]);
 
+            var path = d3.geo.path()
+                .projection(projection);
+
             var svg = d3.select(element[0]).append('svg')
                 .attr('width', width)
                 .attr('height', height);
 
-            var path = d3.geo.path()
-                .projection(projection);
+            var zoom = d3.behavior.zoom()
+                .translate(projection.translate())
+                .scale(projection.scale())
+                .scaleExtent([height, 8 * height])
+                .on('zoom', zoomed);
 
-            
-            // d3.json('/assets/us.json', function(error, topology) {
-            //   console.log(topojson);
-            //   svg.selectAll('path')
-            //     .data(topojson.feature(topology, topology.objects.counties).features)
-            //     .enter().append('path')
-            //     .attr('d', path)
-            //     .on('mouseover', function(data) {
-            //       // Identify county ID
-            //       var countyId = data.id;
-            //       console.log(countyId);
-            //     });
-            // });
+            var g = svg.append('g')
+                .call(zoom);
+
+            g.append("rect")
+                .attr("class", "background")
+                .attr("width", width)
+                .attr("height", height);
+
+
+            function zoomed() {
+              projection.translate(d3.event.translate).scale(d3.event.scale);
+              g.selectAll('path').attr('d', path);
+            }
+
+            d3.json('/assets/us.json', function(error, topology) {
+              console.log(topojson);
+              g.append('g')
+                  .attr('id', 'counties')
+                .selectAll('path')
+                  .data(topojson.feature(topology, topology.objects.counties).features)
+                .enter().append('path')
+                  .attr('d', path)
+                  .on('mouseover', function(data) {
+                    // Identify county ID
+                    var countyId = data.id;
+                    // console.log(countyId);
+                  });
+            });
 
             // Render the zones of drought severity
-            d3.json('/assets/california-drought-topo.json', function(error, topology) {
-            // d3.json('/assets/USDM_20141111-topo.json', function(error, topology) {
-              // Get the first object of data in the file.
-              // console.log(topology);
-              var first = {};
-              for (var key in topology.objects) {
-                first = topology.objects[key];
-                if(typeof(first) !== 'function') {
-                  break;
-                }
-              }
-              // Draw paths into the DOM.
-              svg.selectAll('path')
-                .data(topojson.feature(topology, first).features)
-                .enter()
-                .append('path')
-                .attr('d', path)
-                .attr('class', function(d) {
-                  console.log(d);
-                  return 'severity' + d.id;
-                });
-            });
+            // d3.json('/assets/california-drought-topo.json', function(error, topology) {
+            // // d3.json('/assets/USDM_20141111-topo.json', function(error, topology) {
+            //   // Get the first object of data in the file.
+            //   // console.log(topology);
+            //   var first = {};
+            //   for (var key in topology.objects) {
+            //     first = topology.objects[key];
+            //     if(typeof(first) !== 'function') {
+            //       break;
+            //     }
+            //   }
+            //   // Draw paths into the DOM.
+            //   svg.selectAll('path')
+            //     .data(topojson.feature(topology, first).features)
+            //     .enter()
+            //     .append('path')
+            //     .attr('d', path)
+            //     .attr('class', function(d) {
+            //       console.log(d);
+            //       return 'severity' + d.id;
+            //     });
+            // });
         });
       }
     };
